e7406384fb845ebade062d832ebbe5b2d4b75292
babkin <>
2000-06-08 23:14:06 +0000

    Changes for building on Windows in Cygnus environment.

--- ttf2pt1/cygbuild.bat
+++
@@ -1,6 +0,0 @@
-rem file to build ttf2pt1 with Cygnus GCC on Windows
-rem don't forget to copy CYGWIN1.DLL into C:\WINDOWS
-
-gcc -o ttf2pt1 -DWINDOWS ttf2pt1.c pt1.c t1asm.c ttf.c -lm
-gcc -o t1asm -DWINDOWS -DSTANDALONE t1asm.c
-
