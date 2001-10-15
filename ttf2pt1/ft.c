70aaa7fe844f15e9f5876685c89ca893d9ca5ba2
babkin <>
2001-10-15 02:32:29 +0000

    Changed the headers for FreeType-2.0.4.

--- ttf2pt1/ft.c
+++ ttf2pt1/ft.c
@@ -14,7 +14,7 @@
 #include <sys/types.h>
 #include <freetype/freetype.h>
 #include <freetype/ftglyph.h>
-#include <freetype/ftnames.h>
+#include <freetype/ftsnames.h>
 #include <freetype/ttnameid.h>
 #include <freetype/ftoutln.h>
 #include "pt1.h"
